c3d070b41933f9fb9c193b07b6120956d0c5651e
dsjramesh7 <rameshdussa07@gmail.com>
2026-01-20 10:36:59 +0530

    express and nodemon thing

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+
+const movies = [
+  {
+    title: "One Piece",
+    year: 2000,
+  },
+  {
+    title: "AOT",
+    year: 2014,
+  },
+];
+
+//plain text
 app.get("/", (req, res) => {
   res.send("Hello World from the other side");
 });
+
+//JSON Response
+app.get("/movies", (req, res) => {
+  res.json({
+    success: true,
+    data: movies,
+    totalLengthOfMovies: movies.length,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`The express server is running on ${PORT}`);
 });
