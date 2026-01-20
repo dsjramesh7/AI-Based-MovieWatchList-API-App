e0cd0adcfc423b150e6ba94970bf0c59bc1df900
dsjramesh7 <rameshdussa07@gmail.com>
2026-01-20 10:40:49 +0530

    statusCode

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ app.get("/movies", (req, res) => {
   });
 });
 
+//Json response with status code
+app.get("/moviesWithStatueCode", (req, res) => {
+  res.status(201).json({
+    success: true,
+    data: movies,
+    totalLengthOfMovies: movies.length,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`The express server is running on ${PORT}`);
 });
