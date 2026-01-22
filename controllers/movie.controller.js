30e2696fdaf1c00ab1004374fb8d89b1803e7f1d
dsjramesh7 <rameshdussa07@gmail.com>
2026-01-22 14:01:43 +0530

    structuring file

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -19,4 +19,158 @@ const getAllMoviesStatusCode = (req, res) => {
   });
 };
 
-module.exports = { getAllMoviesStatusCode };
+// post movie thing
+const postMovie = (req, res) => {
+  const data = req.body;
+  if (!data.title || !data.year) {
+    return res.status(400).json({
+      error: true,
+      message: "Title and Year is required",
+    });
+  }
+
+  movies.push(data);
+  res.status(201).json({
+    success: true,
+    message: `${data.title} movie is added successfully`,
+  });
+};
+
+// blockthe url
+const blocked = (req, res) => {
+  res.json({
+    success: true,
+    message: "You will never see this",
+  });
+};
+
+// route paramter/request paramter with one ID
+const getByIdMovie = (req, res) => {
+  console.log("Route Parameter: ", req.params);
+  const id = parseInt(req.params.id);
+  const movie = movies.find((movie) => movie.id === id);
+  if (!movie) {
+    return res.status(404).json({
+      error: true,
+      message: `Movie with id: ${id} is not found`,
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: movie,
+    message: `Movie Fetched Successfully`,
+  });
+};
+
+// route parameter/request parameter with many Id
+const getByIdBothUserandMovie = (req, res) => {
+  console.log("Route Parameters: ", req.params);
+  const userId = parseInt(req.params.userId);
+  const movieId = parseInt(req.params.movieId);
+  const user = users.find((user) => user.id === userId);
+  const movie = movies.find((movie) => movie.id === movieId);
+  if (!user || !movie) {
+    return res.status(404).json({
+      error: true,
+      message: `Movie or user is not found`,
+    });
+  }
+  res.status(200).json({
+    success: true,
+    data: {
+      user: user,
+      movie: movie,
+    },
+    message: `Movie Fetched Successfully`,
+  });
+};
+
+// query string
+const getByQueryStringMethodMovie = (req, res) => {
+  const query = req.query;
+  console.log("Query String: ", query);
+  const title = query.title;
+
+  const movie = movies.find(
+    (movie) => movie.title.toLowerCase() === title.toLowerCase(),
+  );
+  if (!movie) {
+    return res.status(404).json({
+      error: true,
+      message: `Movie not found`,
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: movie,
+    message: `Movie Fetched Successfully`,
+  });
+};
+
+// Patch Method
+const updateCertainThingMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const movie = movies.find((movie) => movie?.id === id);
+  const data = req.body;
+  if (!data?.title) {
+    res.status(400).json({
+      error: true,
+      message: "Movie Title is required",
+    });
+  }
+  console.log("Data: ", data);
+  if (!movie) {
+    res.status(404).json({
+      error: true,
+      message: "Movie Not Found",
+    });
+  }
+  movie.title = data.title;
+  res.status(200).json({
+    success: true,
+    message: "Title Updated Successfully",
+    data: movie,
+  });
+};
+
+// Delete Method
+const deleteMovie = (req, res) => {
+  const data = req.body;
+  const { title } = data;
+  if (!title) {
+    res.status(400).json({
+      error: true,
+      message: "Title is does not exist",
+    });
+  }
+  const movie = movies.find(
+    (movie) => movie?.title.toLowerCase() === title.toLowerCase(),
+  );
+  if (!movie) {
+    res.status(404).json({
+      error: true,
+      message: "Movie Not Found",
+    });
+  }
+  const updateMovies = movies.filter(
+    (movie) => movie?.title.toLowerCase() !== title.toLowerCase(),
+  );
+  res.status(200).json({
+    success: true,
+    message: "Movie Deleted Successfully",
+    movies: updateMovies,
+  });
+};
+
+module.exports = {
+  getAllMoviesStatusCode,
+  postMovie,
+  blocked,
+  getByIdMovie,
+  getByIdBothUserandMovie,
+  getByQueryStringMethodMovie,
+  updateCertainThingMovie,
+  deleteMovie,
+};
--- routes/movie.js
+++ routes/movie.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { getAllMoviesStatusCode } = require("../controllers/movie.controller");
+const {
+  getAllMoviesStatusCode,
+  postMovie,
+  blocked,
+  getByIdMovie,
+  getByIdBothUserandMovie,
+  getByQueryStringMethodMovie,
+  updateCertainThingMovie,
+  deleteMovie,
+} = require("../controllers/movie.controller");
 
 // router.get("/movies", getAllMovies);
-router.get("moviesWithStatusCode", getAllMoviesStatusCode);
+router.get("/moviesWithStatusCode", getAllMoviesStatusCode);
+router.post("/movies", postMovie);
+router.get("/blocked", blocked);
+router.get("/movies/:id", getByIdMovie);
+router.get("/users/:userId/movies/:movieId", getByIdBothUserandMovie);
+router.get("/get-movies", getByQueryStringMethodMovie);
+router.patch("/update-movie/:id", updateCertainThingMovie);
+router.delete("/delete-movie", deleteMovie);
 
 module.exports = router;
