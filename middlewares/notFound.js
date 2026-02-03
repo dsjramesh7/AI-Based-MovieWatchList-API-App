c26ba334f738a80c9114e3919705a79f2a2053e6
dsjramesh7 <rameshdussa07@gmail.com>
2026-02-03 21:40:08 +0530

    routes message thing

--- middlewares/notFound.js
+++ middlewares/notFound.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 
 const notFound = (req, res, next) => {
   res.status(StatusCodes.NOT_FOUND).json({
-    message: "Route does not exist",
+    message: "Route does not exist Try different routes",
   });
 };
 
